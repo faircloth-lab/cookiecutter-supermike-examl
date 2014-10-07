5ad694a4f216cf310054ad73b5366223cb0b74f0
Brant Faircloth <brant@faircloth-lab.org>
2014-10-07 00:00:49 -0500

    use rep and rep_iterator

--- {{cookiecutter.analysis_name}}/{{cookiecutter.bootrep_trees_directory}}/04_bootrep_tree_reps.sh
+++ {{cookiecutter.analysis_name}}/{{cookiecutter.bootrep_trees_directory}}/04_bootrep_tree_reps.sh
@@ -11,9 +11,10 @@ export workdir={{cookiecutter.top_level_directory}}/{{cookiecutter.analysis_name
 export bootrep=$workdir/{{cookiecutter.bootrep_trees_directory}}
 export bootrep_reps=$bootrep/{{cookiecutter.bootrep_trees_reps_directory}}
 export phylip=$workdir/{{cookiecutter.phylip_file}}
+export reps={{cookiecutter.number_of_bootreps}}
 
 # compute some values on the fly
-reps=$(({{cookiecutter.number_of_bootreps}} - 1))
+rep_iterator=$(({{cookiecutter.number_of_bootreps}} - 1))
 
 mkdir -p $bootrep_reps
 cd $bootrep_reps
@@ -22,7 +23,7 @@ date
 # generate $reps bootreps from phylip file
 raxmlHPC-AVX -N $reps -b $RANDOM -f j -m GTRGAMMA -s $phylip -n REPS
 # convert those bootreps to binary format
-for i in {0..$reps};
+for i in {0..$rep_iterator};
 do
     parse-examl -m DNA -s {{cookiecutter.phylip_file}}.BS$i -n BS$i;
 done
