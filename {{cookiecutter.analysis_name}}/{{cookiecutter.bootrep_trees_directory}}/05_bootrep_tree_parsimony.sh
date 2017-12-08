b97103b3e62b05a12e344cbc5ac9f50c68f6b373
Brant Faircloth <brant@faircloth-lab.org>
2017-12-08 08:29:13 -0600

    Change walltime to 16h

--- {{cookiecutter.analysis_name}}/{{cookiecutter.bootrep_trees_directory}}/05_bootrep_tree_parsimony.sh
+++ {{cookiecutter.analysis_name}}/{{cookiecutter.bootrep_trees_directory}}/05_bootrep_tree_parsimony.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #PBS -q single
 #PBS -l nodes=1:ppn=1
-#PBS -l walltime=5:00:00
+#PBS -l walltime=16:00:00
 #PBS -o 05_bootrep_parsimonator.stdout
 #PBS -e 05_bootrep_parsimonator.stderr
 #PBS -N 05_{{cookiecutter.analysis_name}}
